c74c6ed47f50e8be5dc18bc63fb12701ae32361e
Austin Rochford <austin.rochford@gmail.com>
2013-04-25 15:28:42 +0000

    Removed references to /textures

--- site.hs
+++ site.hs
@@ -12,10 +12,6 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "textures/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
     match "css/**" $ do
         route   idRoute
         compile compressCssCompiler
